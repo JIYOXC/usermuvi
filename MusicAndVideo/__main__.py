c355629add0a61226a37a57e4a9337df34c97efc
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-04 12:42:28 +0700

    Update __main__.py

--- MusicAndVideo/__main__.py
+++ MusicAndVideo/__main__.py
@@ -7,8 +7,8 @@ from MusicAndVideo import config
 
 async def main():
     await config.bot.start()
-    await config.call_py.start()
-    await idle()
+     await config.call_py.start()
+     await idle()
 
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
