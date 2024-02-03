69dfe82f7b820bbc02a92a269f5e0caa2d7f0174
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-02-03 23:44:28 +0700

    Update config.py

--- MusicAndVideo/config.py
+++ MusicAndVideo/config.py
@@ -12,8 +12,7 @@ API_ID = int(os.getenv("API_ID", "23781062"))
 API_HASH = os.getenv("API_HASH", "fc7ec0cb472d8f100683162e7a10eba8")
 SESSION = os.getenv(
     "SESSION",
-    "BQAAB_gAWVznjzESSgLvP_FQ8F2yCYsKsiu63x5ZrzmbIcAqaojGdGyAiSyDQMeQHbg3r8eHJ9zQs8CSQUtNQs-_Qqlgumd8GV9YjYtEPqXXCb1PDcE1oF3GNHJMh1BJzEF1CGwNCHDM4wDqxYhWGnvK5afPHzAIvfheFqIKYW4GcvOWhXmesDDNFI9AC1VXMx_3TwPZUO9pPXaxyvAXm1SUDGK6nCf8VwdPU9GGG3m97_qblGpqH5DRBnUtmY4l74G0sC_dsR5HXdixE7sgWyGQUVr72NwGtUrepp49TyITsW6DipCPVHretTtqKzTSJScMawhDc28Lf8l9M26mfm2DxqIjxgAAAAB17DpQAA",
-)
+    "-)
 COMMAND_PREFIXES = list(
     os.getenv("COMMAND_PREFIXES", "( . , : ; _ - + ! ? / P )").split()
 )
